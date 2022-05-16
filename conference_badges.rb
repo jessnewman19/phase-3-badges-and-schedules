6d7fcc0999e45283a5c02247c612d41454db3554
Jessica <jess.newman19@gmail.com>
2022-05-16 12:59:13 -0400

    commit tree f2f3d5c0e12058e2a0968faa57758359f4d598bb

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+require 'pry'
+
+def badge_maker(name = "Arel")
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz" ])
+    array.map do |el|
+        "Hello, my name is #{el}."
+    end
+end 
+
+def assign_rooms (array =["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"])
+    array.map.with_index(1) do |el, num|
+        "Hello, #{el}! You'll be assigned to room #{num}!"
+    end 
+end 
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |badge|
+        puts badge
+    end 
+
+    assign_rooms(attendees).each do |assignment|
+        puts assignment 
+    end 
+end
